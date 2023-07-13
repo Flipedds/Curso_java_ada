5f34b3ca39b693a04ad3e1d008795362d09d62bb
Filipe André <flipeandre12@gmail.com>
2023-07-13 10:26:04 -0300

    feat: new package

---
+++ src/Aula_um/Main.java
@@ -0,0 +1,17 @@
+package Aula_um;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+//        Tipos primitivos
+//        byte -> 8 bits até 127
+        byte variavelByte = 127;
+        System.out.println(variavelByte);
+//        short -> 32768 a 32767
+//        char -> 0 a 65535
+//        int -> 2b a 2b
+//        long -> -9t a 9t
+        int variavelInt = 2147483;
+        System.out.println(variavelInt);
+    }
+}
